1e87facbda8021064dc550a3e334dce603d76bf8
Marco Garlet <g4rl337@C4m3l07.homenet.telecomitalia.it>
2024-12-06 01:43:35 +0100

    clean day5

--- 2024/day05/pt2.py
+++ 2024/day05/pt2.py
@@ -21,7 +21,6 @@ def kahn_algo(rules, page_number):
     grade_rules_nodes = {node: len([edge for edge in level_rules_edges if edge[1]==node]) for node in level_rules_nodes}
     
     solution = [el for el in page_number if el not in level_rules_nodes]
-    #print(grade_rules_nodes)
     while (len(level_rules_nodes)>0):
         for node in level_rules_nodes_copy:
             if grade_rules_nodes[node] == 0 and node not in solution:
@@ -29,19 +28,9 @@ def kahn_algo(rules, page_number):
                 level_rules_nodes.remove(node)
 
                 for edge in [e for e in level_rules_edges if e[0]==node]:
-                    #print('update')
                     grade_rules_nodes[edge[1]]-=1
-                #print(f"solution add {node}, grade {grade_rules_nodes}")
-                #grade_rules_nodes = {node: len([edge for edge in level_rules_edges if edge[1]==node]) for node in level_rules_nodes}
-            #print(solution)
-
-    
-    #print(f"level_rules_edges: {level_rules_edges} level_rules_nodes: {level_rules_nodes} page_number: {page_number}, solution = {solution}")
 
     return solution
-
-    
-
 
 
 def solve(rules, page_numbers):
@@ -51,7 +40,6 @@ def solve(rules, page_numbers):
         highlighted_rules = [r for r in rules if r[0] in page_number and r[1] in page_number]
         for rule in highlighted_rules:
             if page_number.index(rule[0])>page_number.index(rule[1]):
-                #print(f"Non valido page_number: {page_number}, rule: {rule}, {page_number.index(rule[0])},{page_number.index(rule[1])}")        
                 is_valid = True
                 
 
